5218c5c6bd9aab9216efc331ae840af92b1fc451
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-09-18 00:34:56 -0300

    Install script created

---
+++ src/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Cambiarle este nombre? por ese que pide el enunciado para instalarlo qcyo
+
+#Global variables.
+
+export GRUPO="Grupo5" #Maybe instead of having them as env, we should have them locally? I guess this will be required by most of scripts, so its better to have it in the console already exported
+BINDIR="$GRUPO/bin"
+
+#Instalation
+
+
+	
