34aadd9697e4b60619b6234e10b598683ff21984
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-09-18 02:22:38 -0300

    Refactor

--- src/Demonep.sh
+++ src/Demonep.sh
@@ -7,11 +7,11 @@ FILESDIR="$GRUPO/DIRREC"
 
 #Step1, check the env is running
 
-#if ! [ pgrep "my_program" >/dev/null ]
-#	then
-#		echo "El entorno no se encuentra en ejecucion. Para correr el daemon es necesario tener un entorno activo"
-#		exit 1;
-#fi
+if ! [ pgrep "my_program" >/dev/null ]
+	then
+		echo "El entorno no se encuentra en ejecucion. Para correr el daemon es necesario tener un entorno activo"
+		exit 1;
+fi
 
 #Step2, save a cycle counter
 
@@ -26,10 +26,8 @@ for current_file in $FILESDIR/*
 		#Step4, check its a text file
 		mime_type=($(file -i "$current_file" | cut -d' ' -f2)) #Get the info from the current file, pipe it to the stdin of cut and extract the second field delimited by a space
 
-		if [ $mime_type == text/* ] || [ $mime_type == regular ]
+		if ! [ $mime_type == text/* ] && ! [ $mime_type == regular ]
 			then
-				echo "Archivo es de tipo texto: $mime_type"
-			else
 				echo "Archivo rechazado, motivo: no es un archivo de texto"
 				exit 1 #Later see to where send him if it fails
 		fi
@@ -39,6 +37,7 @@ for current_file in $FILESDIR/*
 		if ! [ -s $current_file ]
 			then 
 				echo "Archivo rechazado, motivo: archivo vacio"
+				exit 1 #Same
 		fi
 
 		#Step6, check the format of the file is 'ejecutado_:year_:provcode_:yyyy:mm:dd.csv
