696542ab36eabbc7bc8c28c040037a6d895bbcb6
manuporto <manu.porto94@gmail.com>
2016-09-20 21:28:10 -0300

    Ask for directories

--- src/install.sh
+++ src/install.sh
@@ -8,7 +8,9 @@ export GRUPO="Grupo5" #Maybe instead of having them as env, we should have them 
 RETURN_PTR="" #Used as return variable to avoid using stdout or shared variables
 BINDIR="$GRUPO/bin" #Maybe we should ask here the bindir? kinda $GRUPO/`input_directory "bin"`
 export CONFDIR="$GRUPO/dirconf" #Its protected. Do not change it!
-
+DEF_DIRS=( "Configuracion" "Ejecutables" "Maestros y Tablas"\\
+"Recepcion de Novedades" "Archivos Aceptados" "Archivos Procesados"\\ 
+"Archivos de Reportes" "Archivos de Log" "Archivos Rechazados" )
 #Instalation
 
 #Utils
@@ -18,13 +20,22 @@ export CONFDIR="$GRUPO/dirconf" #Its protected. Do not change it!
 # Return value is stored iniside RETURN_PTR
 function input_directory {
 	local dir_name=""
-	read -p "Defina el directorio de ejecutables ($GRUPO/$1): " dir_name
+    local msg="Defina el directorio de $1 ($GRUPO/$2): "
+	read -p $msg dir_name
 	if [ -z "$dir_name" ]
 		then
+            dir_name=$1
 			RETURN_PTR=$1
 		else
 			RETURN_PTR=$dir_name
 	fi
+    sed -e "\$a$msg$dir_name" instalep.conf
+}
+
+function select_dirs {
+    for ((i=0; i<${#DEF_DIRS[@]}; i+=2)); do
+        input_directory ${#DEF_DIRS[i]} ${#DEF_DIRS[i+1]}
+    done
 }
 
 #Steps
@@ -33,20 +44,24 @@ STEP_CURRENT=0
 STEP_LAST=1
 
 STEP_VERIFY_NOT_INSTALLED=0
-
+STEP_SELECT_DIRS=1
 while [ $STEP_CURRENT -lt $STEP_LAST ] ;do 
 	case $STEP_CURRENT in
 		$STEP_VERIFY_NOT_INSTALLED) 
 			echo "use use use use your imagineiiiiiiiiishon"
-			if [ -r "$CONFDIR/Instalep.conf" ] 
+			if [ -r "$CONFDIR/instalep.conf" ] 
 				then 
-					echo "fiesta"
+					echo "Leer el .conf"
 				else
 					echo "recursamo"
 			fi
 
 			let "STEP_CURRENT++"
 		;;
+
+        $STEP_SELECT_DIRS)
+            select_dirs            
+        ;;
 
 	esac
 done
