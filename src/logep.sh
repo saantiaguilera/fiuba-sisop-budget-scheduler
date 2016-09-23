67330bdcdec7674c0ead9ea5088602c9d37cc31e
mporto <manu.porto94@gmail.com>
2016-09-23 13:04:59 -0300

    logging working

--- src/logep.sh
+++ src/logep.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-while getopts "hc:m:t:" opt; do
+COMMAND=""
+MESSAGE=""
+TYPE=""
+
+while getopts "h?c:m:t:" opt; do
   case "$opt" in
-    h)
-      echo "Help"
+    h|\?)
+      echo "Usage: logep.sh -c command -m 'Message' -t type of message"
+      echo "Example: logep.sh -c movep -m 'File foo moved' -t INF"
       exit 0
       ;;
     c)
       echo "Command: $OPTARG"
+      COMMAND=$OPTARG
       ;;
     m)
       echo "Message: $OPTARG"
+      MESSAGE=$OPTARG
       ;;
     t)
       echo "Message type: $OPTARG"
+      TYPE=$OPTARG
       ;;
   esac
 done
 
-
+FILE="$COMMAND.log"
+echo "`date -u` `whoami` $TYPE $MESSAGE" >> $FILE
