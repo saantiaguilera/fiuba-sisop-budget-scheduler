ae99ca27bd9dbfd13bcfedb9ce6d3dec924b6283
mporto <manu.porto94@gmail.com>
2016-09-23 18:35:16 -0300

    Check log size and reduce it if its very large

--- src/logep.sh
+++ src/logep.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+readonly MAX_LOG_SIZE=1048576 # 1MB
+readonly NO_LINES_TO_KEEP=100
 COMMAND=""
 MESSAGE=""
-TYPE=""
+TYPE="INF"
+
+function max_log_size_reached() {
+  if [ ! -f $1 ]; then
+    return 0
+  fi
+  local size=$(echo "`du -b $1 | cut -f1`")
+  echo $size
+  if [[ size -ge MAX_LOG_SIZE ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function reduce_log_file() {
+  local temp_file="$1_1.log"
+  tail -n $NO_LINES_TO_KEEP $1 > $temp_file
+  mv $temp_file $1 
+}
+
+function write_log_file() {
+  # local file="$DIR_LOG/$1.log"
+  local file="log/$1.log"
+  max_log_size_reached $file
+  if [ $? -gt 0 ]; then
+    reduce_log_file $file
+  fi
+  echo "`date -u` `whoami` $2 $3" >> $file 
+}
 
 while getopts "h?c:m:t:" opt; do
   case "$opt" in
@@ -26,5 +57,5 @@ while getopts "h?c:m:t:" opt; do
   esac
 done
 
-FILE="$COMMAND.log"
-echo "`date -u` `whoami` $TYPE $MESSAGE" >> $FILE
+
+write_log_file $COMMAND $TYPE $MESSAGE
