d82813df2004f0543ec51bf0455a59b7a89a8cc2
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-09-23 13:52:55 -0300

    Init env variables

--- src/Initep.sh
+++ src/Initep.sh
@@ -4,7 +4,7 @@ RETURN_PTR=0
 
 # 1. Verify if env has been initialized
 
-if ! [ pgrep -x "budget_scheduler" > /dev/null ]
+if pgrep -x "budget_scheduler" > /dev/null
 	then
 		echo "Ambiente ya inicializado, para reiniciar termine la sesin e ingrese nuevamente."
 		#TODO Log msg with Logep
@@ -15,13 +15,30 @@ fi
 
 export GRUPO="Grupo5"
 CONF_FILE="$GRUPO/dirconf/EPLAM.conf"
-
-#TODO Read variables below from CONF_FILE
-export LOG_DIR="$GRUPO/DIRLOG"
-LOG="$LOG_DIR/Initep.log"
 export BIN_DIR=""
 export MAE_DIR=""
 export REC_DIR=""
 export OK_DIR=""
 export PROC_DIR=""
 export INFO_DIR=""
+export LOG_DIR=""
+export NOK_DIR=""
+
+while read -r line
+	do
+		case $line in
+			BIN=*) BIN_DIR=$line;;
+			MAE=*) MAE_DIR=$line;;
+			REC=*) REC_DIR=$line;;
+			OK=*) OK_DIR=$line;;
+			PROC=*) PROC_DIR=$line;;
+			INFO=*) INFO_DIR=$line;;
+			LOG=*) LOG_DIR=$line;;
+			NOK=*) NOK_DIR=$line;;
+	esac
+done < $CONF_FILE
+
+LOG="$LOG_DIR/Initep.log"
+#TODO Check for success, log if necessary
+
+# 3. Check permissions
