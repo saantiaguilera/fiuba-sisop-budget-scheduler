e17e4de7e7dbff3d69419086f283b4cdf4f14d7e
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-09-24 15:17:56 -0300

    Basic cycle sleep count

--- src/Demonep.sh
+++ src/Demonep.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+# Dirs
 DIR_REJECTED=$DIRREC
 DIR_ACCEPTED=$ACEPDIR
 DIR_NEWS=$NOVEDIR
-_mov="$BINDIR/MoverA.sh"
-_log="$BINDIR/GraLog.sh"
 
+# Shell scripts
+SH_MOV="$BINDIR/MoverA.sh"
+SH_LOG="$BINDIR/GraLog.sh"
+
+# Sleep time
 TIME_SLEEP=15
 
+# Messages
 MSG_ACCEPTED="Archivo $NOMBRE_ARCHIVO aceptado, movido a $PATH_ACEPTADO"
 MSG_ERR_INVALID_FILE="Tipo de archivo invalido"
 MSG_ERR_INVALID_DATE="Fecha invalida"
 MSG_ERR_OUTOFBOUNDS_DATE="Fecha fuera de rango"
-MSG_ERR_INVALID_COUNTRY_CODE="Central inexistente"
+MSG_ERR_INVALID_COUNTRY_CODE="Codigo de provincia inexistente"
 MSG_ERR_UNKNOWN="Error desconocido"
-MSG_ERR_NOT_INITIALIZED="AFUMB corriendo bajo el no.: $PID"
-MSG_ERR_PID_RUNNING="Invocacion de AFUMB pospuesta para el siguiente ciclo"
+MSG_ERR_NOT_INITIALIZED="#### corriendo bajo el no.: $PID"
+MSG_ERR_PID_RUNNING="Invocacion de #### pospuesta para el siguiente ciclo"
+
+# Initialize cycle
+let "CYCLE_COUNT = 0"
+while true; do
+  CYCLE_NUMBER_MESSAGE="#### ciclo nro. $CYCLE_COUNT"
+ 
+  $SH_LOG "AFREC" "$CYCLE_NUMBER_MESSAGE" "INFO"
+
+  let "CYCLE_COUNT = CYCLE_COUNT + 1"
+
+sleep "$TIME_SLEEP"
+done
+
