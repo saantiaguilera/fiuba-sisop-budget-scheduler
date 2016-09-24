737e0abea41a8ff6e6be9aa7fbe06cccc8a3cb2b
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-09-24 15:21:05 -0300

    getfilessize method

--- src/Demonep.sh
+++ src/Demonep.sh
@@ -6,8 +6,8 @@ DIR_ACCEPTED=$ACEPDIR
 DIR_NEWS=$NOVEDIR
 
 # Shell scripts
-SH_MOV="$BINDIR/MoverA.sh"
-SH_LOG="$BINDIR/GraLog.sh"
+sh_mov="$BINDIR/MoverA.sh"
+sh_log="$BINDIR/GraLog.sh"
 
 # Sleep time
 TIME_SLEEP=15
@@ -22,14 +22,21 @@ MSG_ERR_UNKNOWN="Error desconocido"
 MSG_ERR_NOT_INITIALIZED="#### corriendo bajo el no.: $PID"
 MSG_ERR_PID_RUNNING="Invocacion de #### pospuesta para el siguiente ciclo"
 
+# Returns number of files in the dir passed as param inside FILES_SIZE
+# @Return FILES_SIZE
+function get_files_size() {
+  FILES_SIZE=$(ls -1 $1 | wc -l)
+}
+
 # Initialize cycle
 let "CYCLE_COUNT = 0"
 while true; do
   CYCLE_NUMBER_MESSAGE="#### ciclo nro. $CYCLE_COUNT"
  
-  $SH_LOG "AFREC" "$CYCLE_NUMBER_MESSAGE" "INFO"
+  $sh_log "AFREC" "$CYCLE_NUMBER_MESSAGE" "INFO"
 
   let "CYCLE_COUNT = CYCLE_COUNT + 1"
+
 
 sleep "$TIME_SLEEP"
 done
