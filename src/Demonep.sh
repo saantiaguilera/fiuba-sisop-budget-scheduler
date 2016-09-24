7f8e4150a4761ecc074f308708b5edaa4e1f9659
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-09-24 16:16:35 -0300

    Minor changes

--- src/Demonep.sh
+++ src/Demonep.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Dirs
-DIR_REJECTED=$DIRREC
-DIR_ACCEPTED=$ACEPDIR
-DIR_NEWS=$NOVEDIR
+DIR_REJECTED=$DIRNOK
+DIR_ACCEPTED=$DIROK
+DIR_NEWS=$DIRREC
+DIR_LOG=$DIRLOG
+FILE_LOG="Demonep.log"
 
 # Shell scripts
-sh_mov="$BINDIR/Mov.sh"
-sh_log="$BINDIR/Log.sh"
+sh_mov="$BINDIR/Movep.sh"
+sh_log="$BINDIR/Logep.sh"
+sh_process="$BINDIR/Procep.sh"
 
 # Sleep time
 TIME_SLEEP=15
 
 # Messages
 MSG_ACCEPTED="Archivo $NOMBRE_ARCHIVO aceptado, movido a $PATH_ACEPTADO"
-MSG_ERR_INVALID_FILE="Tipo de archivo invalido"
+MSG_FILE_DETECTED="Archivo detectado: "
+MSG_ERR_INVALID_FILE_TYPE="Archivo rechazado, motivo: no es un archivo de texto"
+MSG_ERR_INVALID_FILE_SIZE="Archivo rechazado, motivo: archivo vacio"
 MSG_ERR_INVALID_DATE="Fecha invalida"
 MSG_ERR_OUTOFBOUNDS_DATE="Fecha fuera de rango"
 MSG_ERR_INVALID_COUNTRY_CODE="Codigo de provincia inexistente"
@@ -22,20 +27,22 @@ MSG_ERR_UNKNOWN="Error desconocido"
 MSG_ERR_NOT_INITIALIZED="Proceso corriendo bajo el no.: $PID"
 MSG_ERR_PID_RUNNING="Invocacion de #### pospuesta para el siguiente ciclo"
 
-# Returns number of files in the dir passed as param inside FILES_SIZE
-# @Return FILES_SIZE
-function get_files_size() {
-  FILES_SIZE=$(ls -1 $1 | wc -l)
-}
-
-# Evicts non text files from the news dir handling the rejected ones.
+# Evicts non text files or empty from the news dir handling the rejected ones.
 function evict_malformed_files() {
 	for FILE in $(ls -1 "$DIR_NEWS");do
+		IS_REJECTED=0
 		if [ $(file "$DIR_NEWS/$FILE" | grep -c "text") = 0 ]
 			then
 				#TODO ver mensajes y el primer parametro del log
-		    	$sh_log "#####" "$MSG_ERR_INVALID_FILE" "INFO"
+		    	$sh_log "$FILE_LOG" "$MSG_ERR_INVALID_FILE_TYPE"
 		    	$sh_mov "$DIR_NEWS/$FILE" "$DIR_REJECTED"
+		    	$IS_REJECTED=1
+		fi
+
+		if [ $IS_REJECTED -eq 0 ] && [ `wc -l $FILE` -eq 0 ]
+			then
+				$sh_log "$FILE_LOG" "$MSG_ERR_INVALID_FILE_SIZE"
+				$sh_mov "$DIR_NEWS/$FILE" "$DIR_REJECTED"
 		fi
 	done
 }
@@ -43,7 +50,7 @@ function evict_malformed_files() {
 # Saves the country codes in array CODES_COUNTRIES
 function parse_country_codes() {
 	# TODO ver el countrydir y codes.csv
-	CODES_COUNTRIES=($(cat "$COUNTRY_DIR/codes.csv" | cut -d \; -f 1))
+	CODES_COUNTRIES=($(cat "$DIRMAE/codes.csv" | cut -d \; -f 1))
 }
 
 # Validates the country code passed as param is inside the country array
@@ -54,49 +61,87 @@ function validate_country_code() {
 	    ;;
 	    *)
 			#TODO check first param of the log
-	        $sh_log "####" "$MSG_ERR_INVALID_COUNTRY_CODE" "INFO"
+	        $sh_log "$FILE_LOG" "$MSG_ERR_INVALID_COUNTRY_CODE"
 	        $sh_mov "$DIR_NEWS/$2" "$DIR_REJECTED"
 	        let "EXIT_CODE = 1"
 	    ;;
   	esac
 }
 
+# Validates date
+function validar_fecha() {
+	M_DATE=$(echo $1 | sed 's/#REGEX FOR DATE#//' | sed 's/#REGEX FOR DATE#//')
+	M_YEAR=$(echo ${M_DATE} | cut -c1-4)
+	M_MONTH=$(echo ${M_DATE} | cut -c5-6)
+	M_DAY=$(echo ${M_DATE} | cut -c7-8)
+
+	# TODO verify this
+	if [ $M_YEAR -lt `date +'%Y'` ]; then
+		$sh_log "$FILE_LOG" "$MSG_ERR_INVALID_DATE"
+		$sh_mov "$DIR_NEWS/$1" "$DIR_REJECTED"
+		let "EXIT_CODE = 1"
+		return
+	else
+		: # Good to go
+	fi
+
+	if [ $(date -d "$M_DATE" +"%Y%b%d" 2>/dev/null 1>/dev/null; echo $?) == 1 ];then
+		$sh_log "$FILE_LOG" "$MENSAJE_FECHA_INVALIDA"
+		$sh_mov "$DIR_NEWS/$1" "$DIR_REJECTED"
+		let "EXIT_CODE = 1"
+	fi
+}
+
+#TODO Check program hasnt been initialized!!
+
 # Initialize cycle
 let "CYCLE_COUNT = 0"
 while true; do
-	CYCLE_NUMBER_MESSAGE="Daemonep ciclo nro. $CYCLE_COUNT"
+	CYCLE_NUMBER_MESSAGE="Demonep ciclo nro. $CYCLE_COUNT"
  
 	# TODO ver mensaje de log
-	$sh_log "####" "$CYCLE_NUMBER_MESSAGE" "INFO"
+	$sh_log "$FILE_LOG" "$CYCLE_NUMBER_MESSAGE"
 
 	let "CYCLE_COUNT = CYCLE_COUNT + 1"
 
-	get_files_size $DIR_NEWS
-	if [ $FILES_SIZE -gt 0 ]
- 		then
-	 		evict_malformed_files
+	evict_malformed_files
 
-	    	parse_country_codes
-			FILES=$(ls $DIR_NEWS)
+	parse_country_codes
+	FILES=$(ls $DIR_NEWS)
+	for FILE in $FILES ;do
+		let "EXIT_CODE = 0"
 
-			for FILE in $FILES ;do
-		    	let "EXIT_CODE = 0"
+		$sh_log "$FILE_LOG" "$MSG_FILE_DETECTED $FILE"
 
-		    	#TODO forgot to validate "a;o presupuestario!"
+		#TODO forgot to validate "a;o presupuestario!"
 
-		    	if [ $EXIT_CODE -eq "0" ]; then
-	        		COUNTRY_CODE=$(echo $file | sed 's/#Regex for getting the code#//' )
-	        		validate_country_code "$COUNTRY_CODE" "$FILE"
-	      		fi
+		if [ $EXIT_CODE -eq "0" ]; then
+	        COUNTRY_CODE=$(echo $file | sed 's/#Regex for getting the code#//' )
+	        validate_country_code "$COUNTRY_CODE" "$FILE"
+	    fi
 
-	      		if [ $EXIT_CODE -eq "0" ]; then
-		    		validate_date
-				fi
+	    if [ $EXIT_CODE -eq "0" ]; then
+		    validate_date "$FILE"
+		fi
+	done
 
-	      	done
-  	fi
-
-
+	hay_archivos $NOVEDADES
+	if [ $cantidad_archivos -eq 0 ];then
+	    # echo $(ls -1 "$ACEPTADOS")
+	    hay_archivos $ACEPTADOS
+	    if [ $cantidad_archivos -gt 0 ];then
+	      # echo "Voy a arrancar AFUMB"
+	      if [[ $(ps -aux | grep -e "[0-9] [a-z]* AFREC" ) == "" ]];then
+	        	# echo "No se esta ejecutando asi que lo arranco"
+	        	bash $BINDIR/Arrancar.sh ./AFUMB
+	        	PID_AFUMB=$(pgrep AFUMB)
+	    	    $GRALOG "AFREC" "AFUMB corriendo bajo el no.: $PID_AFUMB" "INFO"
+		    else
+	        	# echo "Se esta ejecutando asi que lo pospongo"
+	        	$GRALOG "AFREC" "$MENSAJE_AFUMB_OCUPADO" "WAR"
+	        fi
+	    fi
+	fi
 
 
 	sleep "$TIME_SLEEP"
