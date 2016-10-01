1dab19b28ac0c9a331f14f663100ed32ab0aa9bc
saantiaguilera <santiago.aguilera@mercadolibre.com>
2016-10-01 18:04:44 -0300

    Listep initialize

--- src/Listep.pl
+++ src/Listep.pl
@@ -3,3 +3,25 @@
 use v5.10.0;
 use warnings;
 use strict;
+
+sub isInitialized{
+	$MAEDIR=$ENV{'DIRMAE'};
+	if ("$DIRMAE"){
+		return 1;	
+	}else{
+		return 0;
+	}
+	
+}
+
+sub isAlreadyRunning{
+	$counter = `ps -a | grep -c 'Listep'`;
+	#$counterIfIsNotRunning = 2; #Linux
+	$counterIfIsNotRunning = 3; #MAC
+	if($counter > $counterIfIsNotRunning){
+		return 1;
+	}else{
+		return 0;
+	}
+
+}
